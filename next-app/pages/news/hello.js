d25fa5ca461e7d6865a9b67fa77ea07b0ceed4ac
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-01-12 09:30:47 -0500

    wokring on new feature

---
+++ next-app/pages/news/hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function hello() {
+    return (
+        <div>
+            <h2>thisis a demo hello page</h2>
+        </div>
+    )
+}
---
+++ next-app/pages/news/hello2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function hello2() {
+    return (
+        <div>
+            <h3>thsi is a new hellodeomo 2</h3>
+        </div>
+    )
+}
